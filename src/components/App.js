c8ebf2e5be7bb8bcb7e5b8915187825adc22a4d5
manojsingh047 <manojsingh047@gmail.com>
2019-11-16 23:55:06 +0530

    added react router

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 import "./../styles/App.css";
 import { LinkList } from "./LinkList";
+import CreateLink from "./CreateLink";
 
 function App() {
-  return <LinkList />;
+  return (
+    <div>
+      <LinkList />
+      <CreateLink />
+    </div>
+  );
 }
 
 export default App;
