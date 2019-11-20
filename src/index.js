95c2da67e0dc4b30b8195496ec89c65c8ab5c815
manojsingh047 <manojsingh047@gmail.com>
2019-11-21 00:53:50 +0530

    subscription and pagination

--- src/index.js
+++ src/index.js
@@ -10,7 +10,9 @@ import { InMemoryCache } from "apollo-cache-inmemory";
 import { BrowserRouter } from "react-router-dom";
 import { setContext } from "apollo-link-context";
 import { AUTH_TOKEN } from "./constants";
-
+import { split } from "apollo-link";
+import { WebSocketLink } from "apollo-link-ws";
+import { getMainDefinition } from "apollo-utilities";
 const httpLink = createHttpLink({
   uri: "http://localhost:4000"
 });
@@ -23,10 +25,35 @@ const authLink = setContext((_, { headers }) => {
     }
   };
 });
+
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN)
+    }
+  }
+});
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === "OperationDefinition" && operation === "subscription";
+  },
+  wsLink,
+  authLink.concat(httpLink)
+);
+
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link,
   cache: new InMemoryCache()
 });
+
+// const client = new ApolloClient({
+//   link: authLink.concat(httpLink),
+//   cache: new InMemoryCache()
+// });
 
 ReactDOM.render(
   <BrowserRouter>
